f49c9f59ba420ea68df1b335206ba9348c55b38b
Zofia Wisniowolska <zofia.wisniowolska.ctr@sabre.com>
2018-01-26 22:45:27 +0100

    Format game.js

--- js/game.js
+++ js/game.js
@@ -1,44 +1,47 @@
-
 /* Game namespace */
 var game = {
 
-    // an object where to store game information
-    data : {
-        // score
-        score : 0
-    },
+  // an object where to store game information
+  data: {
+    // score
+    score: 0
+  },
 
 
-    // Run on page load.
-    "onload" : function () {
-        // Initialize the video.
-        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto", scaleMethod: 'flex-width'})) {
-            alert("Your browser does not support HTML5 canvas.");
-            return;
-        }
-
-        // Initialize the audio.
-        me.audio.init("mp3,ogg");
-
-        // set and load all resources.
-        // (this will also automatically switch to the loading screen)
-        me.loader.preload(game.resources, this.loaded.bind(this));
-    },
-
-    // Run on game resources loaded.
-    "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
-        me.state.set(me.state.PLAY, new game.PlayScreen());
-
-        // add our player entity in the entity pool
-        me.pool.register("player1Entity", game.PlayerEntity);
-
-        me.input.bindKey(me.input.KEY.LEFT, 'left');
-        me.input.bindKey(me.input.KEY.RIGHT, 'right');
-        me.input.bindKey(me.input.KEY.UP, 'jump');
-        me.input.bindKey(me.input.KEY.DOWN, 'down');
-
-        // Start the game.
-        me.state.change(me.state.PLAY);
+  // Run on page load.
+  "onload": function() {
+    // Initialize the video.
+    if (!me.video.init(960, 640, {
+        wrapper: "screen",
+        scale: "auto",
+        scaleMethod: 'flex-width'
+      })) {
+      alert("Your browser does not support HTML5 canvas.");
+      return;
     }
+
+    // Initialize the audio.
+    me.audio.init("mp3,ogg");
+
+    // set and load all resources.
+    // (this will also automatically switch to the loading screen)
+    me.loader.preload(game.resources, this.loaded.bind(this));
+  },
+
+  // Run on game resources loaded.
+  "loaded": function() {
+    me.state.set(me.state.MENU, new game.TitleScreen());
+    me.state.set(me.state.PLAY, new game.PlayScreen());
+
+    // add our player entity in the entity pool
+    me.pool.register("player1Entity", game.PlayerEntity);
+
+    me.input.bindKey(me.input.KEY.LEFT, 'left');
+    me.input.bindKey(me.input.KEY.RIGHT, 'right');
+    me.input.bindKey(me.input.KEY.UP, 'jump');
+    me.input.bindKey(me.input.KEY.DOWN, 'down');
+
+    // Start the game.
+    me.state.change(me.state.PLAY);
+  }
 };

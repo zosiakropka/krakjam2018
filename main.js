66de38e8c2f599d7b2f9c6dffccaeaa4437872d5
Zofia Wisniowolska <zofia.wisniowolska.ctr@sabre.com>
2018-01-27 12:36:46 +0100

    Enhance the whay game is opened

--- main.js
+++ main.js
@@ -17,7 +17,10 @@ app.on('window-all-closed', () => {
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 960, height: 660 });
+  mainWindow = new BrowserWindow({
+    width: 960,
+    height: 660,
+    fullscreen: true });
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({

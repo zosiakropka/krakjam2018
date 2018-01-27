6d5e155523161b8ff4cfd63425b2fb361bc59354
Zosia Sobocinska <Zosia Sobocinska>
2018-01-27 16:08:03 +0100

    Add assets!

---
+++ data/map/tile_floor_walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tile_floor_walls" tilewidth="32" tileheight="32" tilecount="15" columns="15">
+ <image source="../img/map/tile_floor_walls.png" width="480" height="32"/>
+</tileset>
---
+++ data/map/wall_blue01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="wall_blue01" tilewidth="32" tileheight="32" tilecount="26" columns="26">
+ <image source="../img/map/wall_blue01.png" width="832" height="32"/>
+</tileset>

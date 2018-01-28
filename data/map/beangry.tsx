522ce35afe8aa3358a64c47737da7f4f4c7d668b
Jan Wisniowolski <jesykh@gmail.com>
2018-01-28 12:11:33 +0100

    Add writnigs on the wall!

---
+++ data/map/beangry.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="beangry" tilewidth="32" tileheight="32" tilecount="36" columns="12">
+ <image source="../img/map/beangry.png" width="384" height="96"/>
+</tileset>
---
+++ data/map/stayalive.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="stayalive" tilewidth="32" tileheight="32" tilecount="36" columns="12">
+ <image source="../img/map/stayalive.png" width="384" height="96"/>
+</tileset>

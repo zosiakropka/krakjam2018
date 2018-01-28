5433bf08b83a093b3ae8ba2f270cd1345816f805
Jan Wisniowolski <jesykh@gmail.com>
2018-01-28 10:26:21 +0100

    Change player assets! (and some tiles animation?)

--- data/map/save.tsx
+++ data/map/save.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="save" tilewidth="32" tileheight="32" tilecount="4" columns="2">
  <image source="../img/save.png" width="64" height="66"/>
+ <tile id="3">
+  <animation>
+   <frame tileid="0" duration="150"/>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="2" duration="150"/>
+   <frame tileid="3" duration="150"/>
+  </animation>
+ </tile>
 </tileset>

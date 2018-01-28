18694dce26cb769edd046a057798f2ee0a3e7005
Zosia Wisniowolska <wisniowolska@gmail.com>
2018-01-28 11:51:38 +0100

    Enhance second level

--- data/map/tile_floor_walls.tsx
+++ data/map/tile_floor_walls.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tile_floor_walls" tilewidth="32" tileheight="32" tilecount="18" columns="18">
- <image source="../img/map/tile_floor_walls.png" width="576" height="32"/>
+<tileset name="tile_floor_walls" tilewidth="32" tileheight="32" tilecount="36" columns="18">
+ <image source="../img/map/tile_floor_walls.png" width="576" height="64"/>
 </tileset>

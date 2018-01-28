27b6256ddc86570e8495bf7e8cbf996a6ab661ff
Zosia Wisniowolska <wisniowolska@gmail.com>
2018-01-28 03:02:47 +0100

    Fix the resolution

--- js/game.js
+++ js/game.js
@@ -11,7 +11,7 @@ var game = {
   // Run on page load.
   "onload": function() {
     // Initialize the video.
-    if (!me.video.init(960, 640, {
+    if (!me.video.init(1280, 800, {
         wrapper: "screen",
         scale: "auto",
         scaleMethod: 'flex-width'

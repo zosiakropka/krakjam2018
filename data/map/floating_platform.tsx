aacb64cb3c13a09909fc7391e414b4addab91119
Zosia Wisniowolska <wisniowolska@gmail.com>
2018-02-03 22:14:30 +0100

    Enhance thin platforms

---
+++ data/map/floating_platform.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="floating_platform" tilewidth="32" tileheight="32" tilecount="10" columns="5">
+ <image source="../img/map/floating_platform.png" width="160" height="64"/>
+</tileset>
